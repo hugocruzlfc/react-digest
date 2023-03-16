3a28cbad47b2558b1b0ee31b8aca32358dbee7e4
hde_la_torre <hugo.de_la_torre@sita.aero>
2023-03-16 18:35:57 +0100

    Add svgr example

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# SVGR para convertir iconos svg a compomnentes de react
+
+npm install --save-dev @svgr/cli
+
+Para que el svgr.config.js funcionara se tiene q borrar el 'type:module' del package.json al menos de mommento
---
+++ src/components/SVGR_Icons/index.ts
@@ -0,0 +1,3 @@
+export { default as Icons8Angularjs } from "./Icons8Angularjs";
+export { default as Icons8Reaccionar150 } from "./Icons8Reaccionar150";
+export { default as Icons8Reaccionar80 } from "./Icons8Reaccionar80";
